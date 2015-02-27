982db3aaf0df6054702d7f2124648d522634f541
jianglong0156 <jianglong0156@gmail.com>
2015-02-27 14:38:36 +0800

    add publish

---
+++ js-moonwarriors/publish/localTest1/src/engineAdapter/Adapter.js
@@ -0,0 +1,534 @@
+/**
+ * Created by leo on 2015/1/29.
+ */
+
+var EDV3 = 1;
+var EDV4 = 2;
+var ED = ED || {};
+ED.gameObj = {};
+ED.version = EDV3;
+
+
+if (!cc.sys)
+{
+    ED.version = EDV4;
+    ED.sys = {};
+    cc.sys = ED.sys;
+    (function(sys){
+    /**
+     * Canvas of render type
+     * @constant
+     * @type {Number}
+     */
+    cc._RENDER_TYPE_CANVAS = 0;
+
+    /**
+     * WebGL of render type
+     * @constant
+     * @type {Number}
+     */
+    cc._RENDER_TYPE_WEBGL = 1;
+
+    /**
+     * English language code
+     * @memberof cc.sys
+     * @name LANGUAGE_ENGLISH
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_ENGLISH = "en";
+
+    /**
+     * Chinese language code
+     * @memberof cc.sys
+     * @name LANGUAGE_CHINESE
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_CHINESE = "zh";
+
+    /**
+     * French language code
+     * @memberof cc.sys
+     * @name LANGUAGE_FRENCH
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_FRENCH = "fr";
+
+    /**
+     * Italian language code
+     * @memberof cc.sys
+     * @name LANGUAGE_ITALIAN
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_ITALIAN = "it";
+
+    /**
+     * German language code
+     * @memberof cc.sys
+     * @name LANGUAGE_GERMAN
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_GERMAN = "de";
+
+    /**
+     * Spanish language code
+     * @memberof cc.sys
+     * @name LANGUAGE_SPANISH
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_SPANISH = "es";
+
+    /**
+     * Spanish language code
+     * @memberof cc.sys
+     * @name LANGUAGE_DUTCH
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_DUTCH = "du";
+
+    /**
+     * Russian language code
+     * @memberof cc.sys
+     * @name LANGUAGE_RUSSIAN
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_RUSSIAN = "ru";
+
+    /**
+     * Korean language code
+     * @memberof cc.sys
+     * @name LANGUAGE_KOREAN
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_KOREAN = "ko";
+
+    /**
+     * Japanese language code
+     * @memberof cc.sys
+     * @name LANGUAGE_JAPANESE
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_JAPANESE = "ja";
+
+    /**
+     * Hungarian language code
+     * @memberof cc.sys
+     * @name LANGUAGE_HUNGARIAN
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_HUNGARIAN = "hu";
+
+    /**
+     * Portuguese language code
+     * @memberof cc.sys
+     * @name LANGUAGE_PORTUGUESE
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_PORTUGUESE = "pt";
+
+    /**
+     * Arabic language code
+     * @memberof cc.sys
+     * @name LANGUAGE_ARABIC
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_ARABIC = "ar";
+
+    /**
+     * Norwegian language code
+     * @memberof cc.sys
+     * @name LANGUAGE_NORWEGIAN
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_NORWEGIAN = "no";
+
+    /**
+     * Polish language code
+     * @memberof cc.sys
+     * @name LANGUAGE_POLISH
+     * @constant
+     * @type {Number}
+     */
+    sys.LANGUAGE_POLISH = "pl";
+
+    /**
+     * @memberof cc.sys
+     * @name OS_WINDOWS
+     * @constant
+     * @type {string}
+     */
+    sys.OS_WINDOWS = "Windows";
+    /**
+     * @memberof cc.sys
+     * @name OS_IOS
+     * @constant
+     * @type {string}
+     */
+    sys.OS_IOS = "iOS";
+    /**
+     * @memberof cc.sys
+     * @name OS_OSX
+     * @constant
+     * @type {string}
+     */
+    sys.OS_OSX = "OS X";
+    /**
+     * @memberof cc.sys
+     * @name OS_UNIX
+     * @constant
+     * @type {string}
+     */
+    sys.OS_UNIX = "UNIX";
+    /**
+     * @memberof cc.sys
+     * @name OS_LINUX
+     * @constant
+     * @type {string}
+     */
+    sys.OS_LINUX = "Linux";
+    /**
+     * @memberof cc.sys
+     * @name OS_ANDROID
+     * @constant
+     * @type {string}
+     */
+    sys.OS_ANDROID = "Android";
+    /**
+     * @memberof cc.sys
+     * @name OS_UNKNOWN
+     * @constant
+     * @type {string}
+     */
+    sys.OS_UNKNOWN = "Unknown";
+
+    /**
+     * @memberof cc.sys
+     * @name WINDOWS
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.WINDOWS = 0;
+    /**
+     * @memberof cc.sys
+     * @name LINUX
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.LINUX = 1;
+    /**
+     * @memberof cc.sys
+     * @name MACOS
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.MACOS = 2;
+    /**
+     * @memberof cc.sys
+     * @name ANDROID
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.ANDROID = 3;
+    /**
+     * @memberof cc.sys
+     * @name IPHONE
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.IPHONE = 4;
+    /**
+     * @memberof cc.sys
+     * @name IPAD
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.IPAD = 5;
+    /**
+     * @memberof cc.sys
+     * @name BLACKBERRY
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.BLACKBERRY = 6;
+    /**
+     * @memberof cc.sys
+     * @name NACL
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.NACL = 7;
+    /**
+     * @memberof cc.sys
+     * @name EMSCRIPTEN
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.EMSCRIPTEN = 8;
+    /**
+     * @memberof cc.sys
+     * @name TIZEN
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.TIZEN = 9;
+    /**
+     * @memberof cc.sys
+     * @name WINRT
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.WINRT = 10;
+    /**
+     * @memberof cc.sys
+     * @name WP8
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.WP8 = 11;
+    /**
+     * @memberof cc.sys
+     * @name MOBILE_BROWSER
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.MOBILE_BROWSER = 100;
+    /**
+     * @memberof cc.sys
+     * @name DESKTOP_BROWSER
+     * @constant
+     * @default
+     * @type {Number}
+     */
+    sys.DESKTOP_BROWSER = 101;
+
+    sys.BROWSER_TYPE_WECHAT = "wechat";
+    sys.BROWSER_TYPE_ANDROID = "androidbrowser";
+    sys.BROWSER_TYPE_IE = "ie";
+    sys.BROWSER_TYPE_QQ = "qqbrowser";
+    sys.BROWSER_TYPE_MOBILE_QQ = "mqqbrowser";
+    sys.BROWSER_TYPE_UC = "ucbrowser";
+    sys.BROWSER_TYPE_360 = "360browser";
+    sys.BROWSER_TYPE_BAIDU_APP = "baiduboxapp";
+    sys.BROWSER_TYPE_BAIDU = "baidubrowser";
+    sys.BROWSER_TYPE_MAXTHON = "maxthon";
+    sys.BROWSER_TYPE_OPERA = "opera";
+    sys.BROWSER_TYPE_OUPENG = "oupeng";
+    sys.BROWSER_TYPE_MIUI = "miuibrowser";
+    sys.BROWSER_TYPE_FIREFOX = "firefox";
+    sys.BROWSER_TYPE_SAFARI = "safari";
+    sys.BROWSER_TYPE_CHROME = "chrome";
+    sys.BROWSER_TYPE_LIEBAO = "liebao";
+    sys.BROWSER_TYPE_QZONE = "qzone";
+    sys.BROWSER_TYPE_SOUGOU = "sogou";
+    sys.BROWSER_TYPE_UNKNOWN = "unknown";
+
+    /**
+     * Is native ? This is set to be true in jsb auto.
+     * @memberof cc.sys
+     * @name isNative
+     * @type {Boolean}
+     */
+    sys.isNative = false;
+
+    var browserSupportWebGL = [sys.BROWSER_TYPE_BAIDU, sys.BROWSER_TYPE_OPERA, sys.BROWSER_TYPE_FIREFOX, sys.BROWSER_TYPE_CHROME, sys.BROWSER_TYPE_SAFARI];
+    var osSupportWebGL = [sys.OS_IOS, sys.OS_WINDOWS, sys.OS_OSX, sys.OS_LINUX];
+    var multipleAudioWhiteList = [
+        sys.BROWSER_TYPE_BAIDU, sys.BROWSER_TYPE_OPERA, sys.BROWSER_TYPE_FIREFOX, sys.BROWSER_TYPE_CHROME, sys.BROWSER_TYPE_BAIDU_APP,
+        sys.BROWSER_TYPE_SAFARI, sys.BROWSER_TYPE_UC, sys.BROWSER_TYPE_QQ, sys.BROWSER_TYPE_MOBILE_QQ, sys.BROWSER_TYPE_IE
+    ];
+
+    var win = window, nav = win.navigator, doc = document, docEle = doc.documentElement;
+    var ua = nav.userAgent.toLowerCase();
+
+    /**
+     * Indicate whether system is mobile system
+     * @memberof cc.sys
+     * @name isMobile
+     * @type {Boolean}
+     */
+    sys.isMobile = ua.indexOf('mobile') != -1 || ua.indexOf('android') != -1;
+
+    /**
+     * Indicate the running platform
+     * @memberof cc.sys
+     * @name platform
+     * @type {Number}
+     */
+    sys.platform = sys.isMobile ? sys.MOBILE_BROWSER : sys.DESKTOP_BROWSER;
+
+    var currLanguage = nav.language;
+    currLanguage = currLanguage ? currLanguage : nav.browserLanguage;
+    currLanguage = currLanguage ? currLanguage.split("-")[0] : sys.LANGUAGE_ENGLISH;
+
+    /**
+     * Indicate the current language of the running system
+     * @memberof cc.sys
+     * @name language
+     * @type {String}
+     */
+    sys.language = currLanguage;
+
+    var browserType = sys.BROWSER_TYPE_UNKNOWN;
+    var browserTypes = ua.match(/sogou|qzone|liebao|micromessenger|qqbrowser|ucbrowser|360 aphone|360browser|baiduboxapp|baidubrowser|maxthon|trident|oupeng|opera|miuibrowser|firefox/i)
+        || ua.match(/chrome|safari/i);
+    if (browserTypes && browserTypes.length > 0) {
+        browserType = browserTypes[0];
+        if (browserType == 'micromessenger') {
+            browserType = sys.BROWSER_TYPE_WECHAT;
+        } else if (browserType === "safari" && (ua.match(/android.*applewebkit/)))
+            browserType = sys.BROWSER_TYPE_ANDROID;
+        else if (browserType == "trident") browserType = sys.BROWSER_TYPE_IE;
+        else if (browserType == "360 aphone") browserType = sys.BROWSER_TYPE_360;
+    }
+    /**
+     * Indicate the running browser type
+     * @memberof cc.sys
+     * @name browserType
+     * @type {String}
+     */
+    sys.browserType = browserType;
+
+    // Get the os of system
+    var iOS = ( ua.match(/(iPad|iPhone|iPod)/i) ? true : false );
+    var isAndroid = ua.match(/android/i) || nav.platform.match(/android/i) ? true : false;
+    var osName = sys.OS_UNKNOWN;
+    if (nav.appVersion.indexOf("Win") != -1) osName = sys.OS_WINDOWS;
+    else if (iOS) osName = sys.OS_IOS;
+    else if (nav.appVersion.indexOf("Mac") != -1) osName = sys.OS_OSX;
+    else if (nav.appVersion.indexOf("X11") != -1 && nav.appVersion.indexOf("Linux") == -1) osName = sys.OS_UNIX;
+    else if (isAndroid) osName = sys.OS_ANDROID;
+    else if (nav.appVersion.indexOf("Linux") != -1) osName = sys.OS_LINUX;
+
+    /**
+     * Indicate the running os name
+     * @memberof cc.sys
+     * @name os
+     * @type {String}
+     */
+    sys.os = osName;
+    }(ED.sys));
+}
+else
+{
+    ED.sys = cc.sys;
+    ED.version = EDV3;
+}
+
+ED.LoaderScene = {
+    preload:function(resourceArr, callBackFun, target)
+    {
+        if (ED.version === EDV3)
+        {
+            cc.LoaderScene.preload(resourceArr, callBackFun, target)
+        }
+        else
+        {
+            ED.gameObj.load(resourceArr, callBackFun, target);
+        }
+    }
+};
+
+
+if (ED.version === EDV4) {
+    cc.ENGINE_VERSION = "Cocos2d-JS v4";
+}
+
+// v4 current is same with v3 20150213
+if (ED.version === EDV4)
+{
+    //window["CocosEngine"] = "Cocos2d-JS v4";
+    ED.getUpdateTime = function(dt)
+    {
+        return dt;
+    }
+    ED.schedule = function (target, callbackFun, delayTime)
+    {
+        target.schedule.call(target, callbackFun, delayTime);
+    }
+}
+else
+{
+    ED.getUpdateTime = function(dt)
+    {
+        return dt;
+    }
+    ED.schedule = function (target, callbackFun, delayTime)
+    {
+        target.schedule.call(target, callbackFun, delayTime);
+    }
+}
+
+ED.getRenderStr = function ()
+{
+    if (ED.version === EDV3)
+    {
+        if (cc._renderType == cc._RENDER_TYPE_WEBGL)
+        {
+            return "WebGL";
+        }
+        else
+        {
+            return "Canvas";
+        }
+    }
+    else
+    {
+        if (ED.gameObj._director.getRenderer().getRenderingContext().type === "webgl")
+        {
+            return "WebGL"
+        }
+        else
+        {
+            return "Canvas";
+        }
+
+    }
+};
+
+if (ED.version === EDV3)
+{
+    ED.FastSprite = cc.Sprite;
+}
+else
+{
+    ED.FastSprite = cc.node.FastSprite;
+    //ED.FastSprite = cc.Sprite;
+}
+
+ED.getXMLHttpRequest = function ()
+{
+    if (cc && cc.loader && cc.loader.getXMLHttpRequest)
+    {
+        return cc.loader.getXMLHttpRequest();
+    }
+    else
+    {
+        return window.XMLHttpRequest ? new window.XMLHttpRequest() : new ActiveXObject("MSXML2.XMLHTTP");
+    }
+};
