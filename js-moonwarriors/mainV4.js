4d565ca0d5b3d4caab4726f275201e8d1052c3aa
jianglong0156 <jianglong0156@gmail.com>
2015-02-27 11:14:49 +0800

    modify defaul mode is webgl

--- js-moonwarriors/mainV4.js
+++ js-moonwarriors/mainV4.js
@@ -90,7 +90,7 @@ window.addEventListener("DOMContentLoaded", function() {
             scaleStrategy: "scale_aspect",
             canvasPosition: "center",
             canvasElement: "gameCanvas",
-            renderer:"canvas"
+            renderer:"webgl"
         });
         //resizeWithBrowserSize(true).  --> implicit yes.
         ED.LoaderScene.preload(
